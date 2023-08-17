4c63e85d6c2a21cf3c4e51b024b47b5d1c0ed48a
itastylt <100284073+itastylt@users.noreply.github.com>
2023-08-17 18:53:19 +0300

    WebFlockerWorker class is finished

--- WEB_CRAWLER/web_crawler.py
+++ WEB_CRAWLER/web_crawler.py
@@ -4,24 +4,38 @@
 # Sources: https://www.topcoder.com/thrive/articles/web-crawler-in-python
 
 # Imports
-import requests
 from bs4 import BeautifulSoup
-import time 
-import csv
-import os
+import time, csv, os, re, base64, requests
+from io import BytesIO
+from PIL import Image
 
 class WebFlockerWorker:
-    def __init__(self, csv_read=False,csv_file="links.csv", links=[]):
+    """
+        WebFlockerWorker class
+        ----------------------
+        Description:
+            Represents One Worker Thread
+            Has basic methods that are used to crawl one
+            Car announcment website
+    """
+    def __init__(self, csv_read: bool,csv_file: str, links: list):
         """
         WebFlockerWorker classes constructor
         ------------------------------------
         Args:
-            csv_read:   True if links need to be read from csv file
-            
-            csv_file:   Filename of the csv in which links are located
+            csv_read:   Boolean: 
+                        True if links need to be read from csv file
+                        
+            csv_file:   String: 
+                        Filename of the csv in which links are located
 
-            links:      List of links that is already read
+            links:      List <String>: 
+                        List of links that is already read
+
+        Return:
+            None
         """
+
         # Set user agent's header to Mozilla
         self.header = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36 QIHU 360SE'}
         # Set links array
@@ -30,60 +44,132 @@ class WebFlockerWorker:
         # If we need to read csv,
         # We read it and save list to links
         if(csv_read):
-            print(self.links)
             with open(csv_file, 'r') as file:
-                reader = csv.DictReader(file)
+                reader = csv.reader(file)
                 self.links = list(reader)
-                print(self.links)
 
     def hasLinks(self) -> bool:
         """
         Check if current instance has any links
         ---------------------------------------
-        Returns:
+        Return:
             True:   Current object instance has links
 
             False:  Current object instance has no links
         """
         return len(self.links) > 0
-    
-    def crawl_cars(self, tags, delay_s):
+
+    def crawl_cars(self, table_tags: list, image_tag: dict, csv_file: str, delay_s: int):
+        """
+        Get car information
+        -------------------
+        Args:
+            table_tags:     List<Dictionary>:
+                            Tags that are queried:
+                            "html_tag" is HTML tag selector
+                            "css_class" is class selector
+                            "content" is pattern that is searched
+                            ex. [{"html_tag":"div", "css_class":"class1", "content": "example1"},
+                                 {"html_tag":"div", "css_class":"class2", "content":"example2"}]
+            
+            image_tag:      Dictionary:
+                            Tag that finds image:
+                            "html_tag" is HTML tag selector (expected img)
+                            "css_class" is class selector
+                            ex: {"html_tag":"img", "css_class":"image_class"}
+
+            csv_file:       String:
+                            Filename of the csv in which our data is saved
+
+            delay_s:        Integer:
+                            Delay of the crawler, found in
+                            https://www.*.*/robots.txt
+
+        Return:
+            None
+        """
+
+        # Wait a bit so we don't get banned
+        time.sleep(delay_s)
+        # Get first index of the link stack
         last_index = len(self.links)-1
-        car_link = self.links.pop(last_index)
-        req = requests.get(car_link, headers=self.header)
+        # Pop stack, and save link into memory
+        car_link = self.links.pop(last_index)[0]
+        # Send HTTP GET request to popped link
+        res = requests.get(car_link, headers=self.header)
         # Parse current page as HTML
-        soup = BeautifulSoup(req.content, 'html.parser')
+        soup = BeautifulSoup(res.content, 'html.parser')
+        tag_vals = []
+
         # Get all elements that contains wanted information
-        for tag in tags:
-            item = soup.findAll(tag)
-            print(item)
+        for tag in table_tags:
+            parameter = soup.find(tag["html_tag"],string=re.compile(tag["content"])).parent
+            value = parameter.find(tag["html_tag"], class_=tag["css_class"]).text
+            tag_vals.append(value)
+
+        # Get image URL
+        image_url = soup.find(image_tag["html_tag"], class_=image_tag["css_class"])['src']
+
+        # If image url is valid, Base64 encode it's buffer
+        # And save it to CSV file, so we can work with it later. 
+        if(image_url != None):
+            time.sleep(5)
+            response = requests.get(image_url, headers=self.header)
+            img = Image.open(BytesIO(response.content))
+
+            # Convert image to cStringIO Buffer.
+            # Get it's raw value, convert it to PNG.
+            # And then do Base64 encode.
+            # --------------------------------------
+            # Note: BytesIO buffer in Python3 is eq to cStringIO buffer in Python2
+            with BytesIO() as buf:
+                img.save(buf, 'png')
+                image_bytes = buf.getvalue()
+                base64_image = base64.b64encode(image_bytes)
+                tag_vals.append(base64_image)
+            # Append csv file with extracted information.
+            with open(csv_file, 'a') as file:
+                writer = csv.writer(file)
+                writer.writerow(tag_vals)
+        # Proceed to the next car link in the stack
+        self.crawl_cars(table_tags=table_tags, image_tag=image_tag, csv_file=csv_file, delay_s=delay_s)
         
-    def crawl_links(self, csv_file="mirror1.csv", base_url="https://autogidas.lt/", url_split=True, pagination_url="https://autogidas.lt/skelbimai/automobiliai/?f_50=kaina_asc&page=",
-    pages=270, html_tag="a", link_class="item-link", delay_s=30):
+    def crawl_links(self, csv_file: str, base_url: str, url_split: bool, pagination_url:str,
+    pages:int, html_tag: str, link_class: str, delay_s: int):
         """ 
         Get car announcment links
         -------------------------
         Args:
-            csv_file:       Filename of the the file, in which
+            csv_file:       String:
+                            Filename of the the file, in which
                             car announcement links are saved
         
-            base_url:       Base url of the site that is getting crawled
+            base_url:       String:
+                            Base url of the site that is getting crawled
 
-            url_split:      True if the crawled link is in format /.../
+            url_split:      Boolean:
+                            True if the crawled link is in format /.../ 
                             False if the crawled link is in format base_url/.../
 
-            pagination_url: Url that represents pagination of the announcments
+            pagination_url: String:
+                            Url that represents pagination of the announcments
 
-            pages:          Number of pages that will be read
+            pages:          Integer:
+                            Number of pages that will be read
 
-            html_tag:       HTML tag of the element in which link
+            html_tag:       String:
+                            HTML tag of the element in which link
                             for the car announcment is saved
 
-            link_class:     Class that is searched by BeautifulSoup
+            link_class:     String:
+                            Class that is searched by BeautifulSoup
                             and we extract link from it
 
-            delay_s:       Delay of the crawler, found in
+            delay_s:        Integer:
+                            Delay of the crawler, found in
                             https://www.*.*/robots.txt
+        Return:
+            None
         """
          
         # Open (create) csv file with writing mode
@@ -94,9 +180,9 @@ class WebFlockerWorker:
             # Crawl for each page
             for i in range(1, pages):
                 # Get current page
-                req = requests.get(pagination_url+str(i), headers=self.header)
+                res = requests.get(pagination_url+str(i), headers=self.header)
                 # Parse current page as HTML
-                soup = BeautifulSoup(req.content, 'html.parser')
+                soup = BeautifulSoup(res.content, 'html.parser')
                 # Get all elements that contains wanted information
                 a = soup.findAll(html_tag, class_=link_class, href=True)
                 
@@ -115,6 +201,9 @@ class WebFlockerServer:
         self.threads = NUM_THREADS
     def test(self):
         mirror1 = WebFlockerWorker(csv_read=True, csv_file="mirror1.csv")
-
+        mirror1.crawl_cars([{"html_tag": "div", "css_class": "right", "content": "Markė"},
+                            {"html_tag": "div", "css_class": "right", "content": "Modelis"}, 
+                            {"html_tag": "div", "css_class": "right", "content": "Metai"}],
+                            {"html_tag": "img", "css_class": "show"}, "autogidas.csv", 30)
 webflockerserver = WebFlockerServer()
 webflockerserver.test()
